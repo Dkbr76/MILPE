f406fdb54542c59e52c8f3eb375f4533841e023a
Dkbr <dkbr76@gmail.com>
2026-02-05 19:33:00 +0900

    Add files via upload

---
+++ Func/MILPE.m
@@ -0,0 +1,78 @@
+
+%%
+%
+%
+%
+%
+%
+%
+%                     
+%           _____ ______   ___  ___       ________  _______        
+%          |\   _ \  _   \|\  \|\  \     |\   __  \|\  ___ \     
+%          \ \  \\\__\ \  \ \  \ \  \    \ \  \|\  \ \   __/|    
+%           \ \  \\|__| \  \ \  \ \  \    \ \   ____\ \  \_|/__  
+%            \ \  \    \ \  \ \  \ \  \____\ \  \___|\ \  \_|\ \ 
+%             \ \__\    \ \__\ \__\ \_______\ \__\    \ \_______\
+%              \|__|     \|__|\|__|\|_______|\|__|     \|_______|
+%                                                      
+%            MILPE 
+%            Apr.2024
+%
+%
+%            *Author info:    
+%            Dkbr(Dkbr76@gmail.com)
+%
+%            *MILPE Preprint:
+%            https://www.researchgate.net/profile/Dk-Br   
+%           
+%            *Description:
+%            MILPE is an algorithm that effectively reconstructs the
+%            original governing equation of data. The purpose is to seek 
+%            a multivariate regression model (or multi-input--multi-output 
+%            function) which has extrapolation capability, ultimately. 
+%            In order to have a sufficient regression capability, 
+%            the Hilbert space where the eigenvectors are extracted from 
+%            should be multidimensional linear.
+%
+%            *Keywords:
+%            #MILPE #SVD #LinearRepresentation 
+%            #SystemIdentification #ROM #Koopman
+%
+%
+%
+%
+%
+%
+
+%ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø%
+%                                                                     %
+%                                                                     %
+%                     (ex) MILPE as a Function                        %
+%                                                                     %
+%                                                                     %
+%ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø,¸,ø¤°º¤ø%
+
+
+function UyUxP = MILPE(X,Y,nMe) 
+
+    % Preprocess
+    Z         = [X;Y];
+    nX        = size(X,1);
+
+    % MILPE
+    [U, S, V] = svd(Z,'econ');
+    Ux        = U(   1:nX  ,  1:nX-nMe);
+    Uy        = U(nX+1:end ,  1:nX-nMe);
+    UyUxP     = Uy*pinv(Ux);
+
+end
+
+% X     =  Input Subspace           (Row: vars. Col: snapshots)
+% Y     =  Output Subspace          (Row: vars. Col: snapshots)
+% Z     =  Unified Solution space   (Row: vars. Col: snapshots)
+% nX    =  Number of vars in input subspace        
+% nMe   =  Number of modes excluded               
+% Ux    =  Projection matrix on input subspace    
+% Uy    =  Projection matrix on output subspace    
+% UyUxP =  Governing Equation Approximated by MILPE
+
