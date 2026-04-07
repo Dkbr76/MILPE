125556fedfbbcca472c8a5cbce79a8de91d50cea
Dkbr <dkbr76@gmail.com>
2026-04-07 15:45:05 +0900

    Update main.m

--- Demo/103_RectCyl/main.m
+++ Demo/103_RectCyl/main.m
@@ -10,8 +10,7 @@
 % MILPE demo - CFD Pressure field regression and prediction 
 % Mar.2026 https://www.mdpi.com/3762868
 % YouTube: 
-https://www.youtube.com/watch?v=gvJTZSLJ0lc
-https://www.youtube.com/watch?v=Oc1LzhAe6wk
+% https://www.youtube.com/watch?v=gvJTZSLJ0lc
 
 clear all; close all;
 
