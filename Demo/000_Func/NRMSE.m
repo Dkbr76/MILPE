20bc97bf61c117edfd8bde978ce7817ddb9e8ef6
Dkbr <dkbr76@gmail.com>
2026-05-13 15:52:13 +0900

    Update NRMSE.m

--- Demo/000_Func/NRMSE.m
+++ Demo/000_Func/NRMSE.m
@@ -1,1 +1,28 @@
+%
+%
+%
+%
+%
+%
+%
+%
+%
+function R = NRMSE(a,b)
 
+    % NRMSE
+    D     = b - a;
+    NUMER = sqrt( sum(sum(D.^2,2)) / ( size(D,1)*size(D,2) ) ); %numerator
+    DENOM = sqrt( sum(sum(a.^2,2)) / ( size(a,1)*size(a,2) ) ); %denomenator
+    R     = NUMER/DENOM;
+
+end
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
