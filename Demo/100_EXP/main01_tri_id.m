c270287c8f74673a59e7ba273300686767fa74c7
Dkbr <dkbr76@gmail.com>
2026-05-15 22:24:24 +0900

    Update main01_tri_id.m

--- Demo/100_EXP/main01_tri_id.m
+++ Demo/100_EXP/main01_tri_id.m
@@ -10,7 +10,7 @@
 % MILPE principle - 1
 % 2 0 2 6 . 0 5 
 % Reconstruct cos(t) with sin(t): 
-%   - same as triagonal identity and probably related to time-delay problem?
+%   - same as triagonal identity (yet data-driven) and probably related to time-delay problem?
 
 clear all; close all;
 
@@ -23,22 +23,26 @@ clear all; close all;
 te = 1e+1;                  % simulation duration
 dt = 1e-2;                  % time-step
 m  = floor((te)/dt+1);      % number of snapshots
-
-% time-vector
-t  = [0:m]*dt;
+t  = [0:m-1]*dt;            % time-vector
 
 % input time-history(s)
 x1 = sin(t);                % <- baseline
-x2 = sin(t + 1e-13);        % variant
+x2 = sin(t + 1e-8);        % variant
+
+% output time-history(s)
+y1 = cos(t);
 
 % input-subspace
 X  = [x1;x2];
 
 % output-subspace
-Y  = cos(t);
+Y  = [y1];
 
 % MILPE
 UyUxP = MILPE(X,Y,0)
+
+% LSQ
+a1 = L2(X,Y)
 
 % sav time-history
 TH0 = [t;X;Y];
@@ -49,30 +53,8 @@ TH0 = [t;X;Y];
 
 
 % MILPE-Prediction
-
-% time-loop
-for it=1:m
-
-    % echo
-    if ( it == 1 ) cnt = 20; end % counter
-    if ( mod(it,0.2*m) < 1 ) disp("MILPE......"+cnt+"%"); cnt = cnt+20; end
-
-    % input snapshot X
-    X = [ x1(it) x2(it) ];
-
-    % output snapshot Y
-    Y = UyUxP * X';
-
-    % store snapshot info
-    SNP = [it*dt X Y]';
-
-    % init TH at it==1
-    if ( it == 1 ) TH1 = zeros(size(SNP,1),m); end
-
-    % save as time-history
-    TH1(:,it) = SNP;
-
-end
+y1_MILPE = UyUxP * X;
+y1_LSQ   = a1    * X;
    
 
 
@@ -81,10 +63,11 @@ end
 
 % time history - x-position
 figure(11)
-plot(TH0(1,:),TH0(  2,:),'c'); hold on;   % OG - x1
-plot(TH0(1,:),TH0(  3,:),'b--'); hold on; % OG - x2
-plot(TH0(1,:),TH0(end,:),'k'); hold on;   % OG - y
-plot(TH1(1,:),TH1(end,:),'m--');          % MILPE - y
+plot(t,x1,'c'); hold on;   % OG - x1
+plot(t,x2,'b--'); hold on; % OG - x2
+plot(t,y1,'k'); hold on;   % OG - y
+plot(t,y1_MILPE,'y--');    % MILPE - y
+plot(t,y1_LSQ,'r--');      % MILPE - y
 xlabel('t');
 ylabel('vars');
 
